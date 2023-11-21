61ae717b1e8a1eaaccb4e4c29968242d510996b0
MrArcano <salvatore-dario@hotmail.it>
2023-11-21 16:47:07 +0100

    [edit]: departing trains query

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -10,7 +10,13 @@ class PageController extends Controller
 {
     public function index(){
         $trains = Train::all();
-        $departing_trains = Train::whereRaw('DATEDIFF(date,current_date) > 0 AND !cancellato')->get();;
+
+        // $departing_trains = Train::whereRaw('DATEDIFF(date,current_date) > 0 AND !cancellato')->get();
+
+        $departing_trains = Train::where('date','>=',date("Y-m-d"))
+                                ->where('cancellato',0)
+                                ->get();
+
         return view("home", compact("trains","departing_trains"));
     }
 }
