6e962f0adffc4b0de1efb65820ee36122164a386
MrArcano <salvatore-dario@hotmail.it>
2023-11-21 15:39:56 +0100

    [edit]: stamp data trains

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,77 @@
 @extends('layouts.main')
 
-@dump($trains)
-@dump($departing_trains)
-
-
 @section('content')
-    <main >
+    <main class="my-5">
         <div class="container">
-            <h1 class="text-center">Lista Treni</h1>
-            <ul class="list-group">
-                @foreach ($trains as $train)
-                    <li class="list-group-item">{{ $train->Codice_Treno }}</li>
-                @endforeach
-            </ul>
+            <h1 class="text-center mb-3">Lista Treni</h1>
+            <table class="table table-dark table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Codice</th>
+                    <th scope="col">Azienda</th>
+                    <th scope="col">From</th>
+                    <th scope="col">To</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Departure time</th>
+                    <th scope="col">Arrival time</th>
+                    <th scope="col">In Time</th>
+                    <th scope="col">Deleted</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($trains as $train)
+                    <tr>
+                        <th scope="row">{{$train->id}}</th>
+                        <td>{{$train->Codice_Treno}}</td>
+                        <td>{{$train->Azienda}}</td>
+                        <td>{{$train->Stazione_di_partenza}}</td>
+                        <td>{{$train->Stazione_di_arrivo}}</td>
+                        <td>{{$train->date}}</td>
+                        <td>{{$train->Orario_di_partenza}}</td>
+                        <td>{{$train->Orario_di_arrivo}}</td>
+                        <td>{{$train->In_orario ? 'True' : 'False'}}</td>
+                        <td>{{$train->Cancellato ? 'True' : 'False'}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
 
-            <h1 class="text-center">Lista Treni in partenza</h1>
-            <ul class="list-group">
-                @foreach ($departing_trains as $train)
-                    <li class="list-group-item">{{ $train->Codice_Treno }}</li>
-                @endforeach
-            </ul>
+            </table>
+
+            <h1 class="text-center mb-3">Lista Treni in partenza</h1>
+            <table class="table table-dark table-striped">
+                <thead>
+                <tr>
+                    <th scope="col">#id</th>
+                    <th scope="col">Codice</th>
+                    <th scope="col">Azienda</th>
+                    <th scope="col">From</th>
+                    <th scope="col">To</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Departure time</th>
+                    <th scope="col">Arrival time</th>
+                    <th scope="col">In Time</th>
+                    <th scope="col">Deleted</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($departing_trains as $train)
+                    <tr>
+                        <th scope="row">{{$train->id}}</th>
+                        <td>{{$train->Codice_Treno}}</td>
+                        <td>{{$train->Azienda}}</td>
+                        <td>{{$train->Stazione_di_partenza}}</td>
+                        <td>{{$train->Stazione_di_arrivo}}</td>
+                        <td>{{$train->date}}</td>
+                        <td>{{$train->Orario_di_partenza}}</td>
+                        <td>{{$train->Orario_di_arrivo}}</td>
+                        <td>{{$train->In_orario ? 'True' : 'False'}}</td>
+                        <td>{{$train->Cancellato ? 'True' : 'False'}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
         </div>
     </main>
 @endsection
